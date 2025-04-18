6ffb52ec55a41077bac6495e083201e836ae5193
Ivar Björling <ivarbjorling@hotmail.com>
2025-04-18 22:46:11 +0200

    Add objects to the scene
    Added some nice glb models with pbr materials

--- src/app.ts
+++ src/app.ts
@@ -28,12 +28,6 @@ class App {
         await this.engine.initAsync();
         this.scene = new BABYLON.Scene(this.engine);
 
-        // populate the scene
-        var camera = new BABYLON.ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, BABYLON.Vector3.Zero(), this.scene);
-        camera.attachControl(this.canvas, true);
-        var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0), this.scene);
-        var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", { diameter: 1 }, this.scene);
-
         // hide/show the Inspector
         window.addEventListener("keydown", (ev) => {
             // Shift+Ctrl+Alt+I
@@ -46,10 +40,59 @@ class App {
             }
         });
 
+        // populate the scene
+        await this.populateScene();
+
         // run the main render loop
         this.engine.runRenderLoop(() => {
             this.scene.render();
         });
+    }
+
+    async populateScene() {
+        // camera
+        var camera = new BABYLON.ArcRotateCamera("camera", Math.PI * 0.15, Math.PI * 0.35, 5, new BABYLON.Vector3(0,1,0), this.scene);
+        camera.attachControl(this.canvas, true);
+        camera.minZ = 0.01;
+        camera.maxZ = 20;
+        camera.zoomOnFactor = 0.5;
+        camera.lowerRadiusLimit = 2;
+        camera.upperRadiusLimit = 8;
+        
+        // skybox
+        const envTex = BABYLON.CubeTexture.CreateFromPrefilteredData("./environment/brown_photostudio_02_4k.env", this.scene); // [source: https://polyhaven.com/a/brown_photostudio_02], converted from hdr to env using https://www.babylonjs.com/tools/ibl/
+        this.scene.environmentTexture = envTex;
+        this.scene.environmentIntensity = 1;
+        const skybox = this.scene.createDefaultSkybox(envTex, true);
+
+        // sphere
+        const sphere = BABYLON.MeshBuilder.CreateSphere("sphere", { diameter: 0.5 }, this.scene);
+        const sphereMaterial = new BABYLON.PBRMaterial("sphereMaterial", this.scene);
+        sphere.position = new BABYLON.Vector3(0, 2, 0);
+        sphereMaterial.roughness = 0;
+        sphere.material = sphereMaterial;
+
+        // ground
+        const ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 15, height: 15 }, this.scene);;
+        const groundMaterial = new BABYLON.PBRMaterial("groundMaterial", this.scene);
+        groundMaterial.roughness = 0.1;
+        ground.material = groundMaterial;
+
+        // table
+        const tableImportResult = await BABYLON.ImportMeshAsync("./models/side_table_tall_01_1k.glb", this.scene); //  [source: https://polyhaven.com/a/side_table_tall_01], convert to glb using blender.
+        const table = tableImportResult.meshes[1] as BABYLON.Mesh;
+        table.position = new BABYLON.Vector3(0, 0, 0);
+
+        // chair
+        const chairImportResult = await BABYLON.ImportMeshAsync("./models/arm_chair_01_1k.glb", this.scene); //  [source: https://polyhaven.com/a/ArmChair_01], convert to glb using blender.
+        const chair = chairImportResult.meshes[1] as BABYLON.Mesh;
+        chair.position = new BABYLON.Vector3(0, 0, -1);
+
+        // shelf
+        const shelfImportResult = await BABYLON.ImportMeshAsync("./models/steel_frame_shelves_01_1k.glb", this.scene); //  [source: https://polyhaven.com/a/steel_frame_shelves_01], convert to glb using blender.
+        const shelf = shelfImportResult.meshes[1] as BABYLON.Mesh;
+        shelf.position = new BABYLON.Vector3(1, 0, 0);
+        shelf.rotation = new BABYLON.Vector3(0, Math.PI / 2, 0);
     }
 }
 
